407770be184852ff7a2a595e751a9f5d42e748fa
abhishek <111995abhishek@gmail.com>
2019-09-18 10:07:18 +0530

    print-statement

---
+++ seperators1.py
@@ -0,0 +1,8 @@
+name="abhishek"
+age=18
+salary=100000
+hair_color="black"
+height="five feet"
+weight=65
+education_degree="b.e"
+print(name,age,salary,hair_color,height,weight,education_degree,sep=",")
