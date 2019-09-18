45fc8b965127cd211b61d97b1750a7aaf61529ff
abhishek <111995abhishek@gmail.com>
2019-09-18 09:52:33 +0530

    print-statement

---
+++ print-formatting.py
@@ -0,0 +1,13 @@
+name=" abhishek"
+age=18
+salary="100000"
+hair_color="black"
+height="five feet"
+weight= 65
+education_degree="b.e"
+print(name)
+print(age)
+print(salary)
+print(hair_color)
+print(height)
+print(weight)
