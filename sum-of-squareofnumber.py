4147c3efd1ea6e1238aefa8c10d6413b27d008a1
abhishek <111995abhishek@gmail.com>
2019-09-18 14:13:26 +0530

    arithmetic-operation

---
+++ sum-of-squareofnumber.py
@@ -0,0 +1,5 @@
+n=int(input("enter number"))
+sum_of_square=0
+for value in range(1,n+1):
+    sum_of_square=value**2+sum_of_square
+print(sum_of_square)
---
+++ sum_of_numbers.py
@@ -0,0 +1,7 @@
+n=int(input("enter the number: "))
+sum=0
+for value in range(1,n+1):
+    sum=sum+value
+    value=value+1
+print("sum is: ",sum)
+    
