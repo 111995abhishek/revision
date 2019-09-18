79b90d2ca1b2a3d8b6702de0d0abad079a05f795
abhishek <111995abhishek@gmail.com>
2019-09-18 09:39:27 +0530

    print-statement

---
+++ prints.py
@@ -0,0 +1,14 @@
+name="abhishek"
+age=18
+salary=100000
+hair_color="black"
+height="five feet"
+weight=65
+education_degree="b.e"
+print("my name  is: ",name)
+print("my age is: ",age)
+print("my salary is: ",salary)
+print("my hair color is: ",hair_color)
+print("my height is: ",height)
+print("my weight is: ",weight)
+print("my degree is: ",education_degree)
